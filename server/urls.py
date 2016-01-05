d3ecb4a815f251c4d9f724b80f0055bd3ffe4b12
brett1479 <brett.bernstein@gmail.com>
2016-01-05 07:14:30 -0500

    Fixes config for angular

--- server/urls.py
+++ server/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.shortcuts import render
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^$', render, {'template_name': 'index.html'})
     
 ]
