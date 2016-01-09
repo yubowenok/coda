2eb32d42ffdcc4b955c34337fc92f2ca3abee586
brett1479 <brett.bernstein@gmail.com>
2016-01-09 06:02:06 -0500

    Removes rest login api

--- api/api/urls.py
+++ api/api/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     url(r'^auth/', include('codaauth.urls')),
     url(r'^problem/', include('codaproblem.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
