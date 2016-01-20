e3481e9e005b96bae690f760f511f845a3b1d459
Brett Bernstein <brett.bernstein@gmail.com>
2016-01-20 13:49:11 -0500

    Actually adds submission and scorecard

---
+++ api/codascorecard/models.py
@@ -0,0 +1,34 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+from django.contrib.auth.models import User, Group
+from codacontest.models import *
+from codaproblem.models import *
+from codasubmission.models import *
+from api.constants import DEFAULT_MAX_LENGTH, MAIN_CLASS_LENGTH, RESULT_LENGTH
+
+class ContestScorecard(models.Model) :
+    user = models.ForeignKey(
+        User,
+        on_delete = models.PROTECT,
+    )
+    contest = models.ForeignKey(
+        Contest,
+        on_delete = models.CASCADE
+    )
+    score = models.IntegerField(default = 0)
+    submissions = models.IntegerField(default = 0)
+
+class ProblemScorecard(models.Model) :
+    contestProblem = models.ForeignKey(
+        ContestProblem,
+        on_delete = models.CASCADE
+    )
+    contestScorecard = models.ForeignKey(
+        ContestScorecard,
+        on_delete = models.CASCADE
+    )
+    score = models.IntegerField(default = 0)
+    submissions = models.IntegerField(default = 0)
+
---
+++ api/codasubmission/models.py
@@ -0,0 +1,107 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+from django.contrib.auth.models import User, Group
+from codacontest.models import *
+from codaproblem.models import *
+from api.constants import DEFAULT_MAX_LENGTH, MAIN_CLASS_LENGTH, RESULT_LENGTH
+
+
+def getBasePath(user, contestProblem, contest) :
+    return 'submissions/%s/%s/%s/%s/%s' % (str(contest.id),
+                                           str(contestProblem.contestProblemID),
+                                           str(username),
+                                           str(instance.id))
+    
+def getSubmissionPath(sub, filename) :
+    prob = sub.problem
+    base = getBasePath(sub.user, prob, prob.contest)
+    return '%s/%s' % (base, filename)
+
+def getUserOutputPath(testFileResult, filename) :
+    testFileID = testFileResult.testFile.testFileID
+    sub = testFileResult.submission
+    prob = sub.problem 
+    base = getBasePath(sub.user, prob, prob.contest)
+    return '%s/%s/%s' % (base, str(testFileID), filename)
+
+class TestFileResultType(models.Model) :
+    name = models.CharField(max_length = RESULT_LENGTH)
+    isSuccess = models.BooleanField()
+    isPending = models.BooleanField()
+
+class ResultType(models.Model) :
+    name = models.CharField(max_length = RESULT_LENGTH)
+    isSuccess = models.BooleanField()
+    isPending = models.BooleanField()
+
+class ContestSubmission(models.Model) :
+    user = models.ForeignKey(
+        User,
+        on_delete = models.PROTECT,
+    )
+    submissionTime = models.DateTimeField(auto_now_add = True)
+    problem = models.ForeignKey(
+        ContestProblem,
+        on_delete = models.CASCADE,
+        related_name = 'submissions',
+    )
+    source = models.FileField(
+        upload_to = getSubmissionPath
+    )
+    language = models.ManyToManyField(Language)
+    mainClass = models.CharField(max_length = MAIN_CLASS_LENGTH)
+
+
+class ProblemResult(models.Model) :
+    submission = models.ForeignKey(
+        ContestSubmission,
+        on_delete = models.CASCADE
+    )
+    contestProblem = models.ForeignKey(
+        ContestProblem,
+        on_delete = models.CASCADE
+    )
+    result = models.ForeignKey(
+        ResultType,
+        on_delete = models.PROTECT
+    )
+    score = models.IntegerField(default = 0)
+
+class BatchResult(models.Model) :
+    contestBatch = models.ForeignKey(
+        ContestBatch,
+        on_delete = models.CASCADE
+    )
+    result = models.ForeignKey(
+        ResultType,
+        on_delete = models.PROTECT
+    )
+    problemResult = models.ForeignKey(
+        ProblemResult,
+        on_delete = models.CASCADE
+    )
+    score = models.IntegerField(default = 0)
+    
+class TestFileResult(models.Model) :
+    testFile = models.ForeignKey(
+        TestFile,
+        on_delete = models.PROTECT
+    )
+    resultTime = models.DateTimeField(auto_now_add = True)
+    userOutput = models.FileField(
+        upload_to = getUserOutputPath
+    )
+    result = models.ForeignKey(
+        TestFileResultType,
+        on_delete = models.PROTECT
+    )
+    memoryUsedBytes = models.BigIntegerField()
+    timeUsedMS = models.BigIntegerField()
+    batchResult = models.ForeignKey(
+        BatchResult,
+        on_delete = models.CASCADE
+    )
+
+
